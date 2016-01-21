e4b44edd654d4229f3955a0a319a833fc43202b1
Nic Smith <48gnomes@gmail.com>
2016-01-20 19:43:38 -0500

    Made New Class, Created object array, wrote records

---
+++ cis237inclass1/Employee.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cis237inclass1
+{
+    class Employee
+    {
+        //Backing fields
+        private string firstNameString;
+        private string lastNameString;
+        private decimal weeklySalaryDecimal;
+
+        //getters and setters
+        public string FirstNameString
+        {
+            get { return firstNameString; }
+            set { firstNameString = value; }
+        }
+
+        public string LastNameString
+        {
+            get { return lastNameString; }
+            set { lastNameString = value; }
+        }
+
+        public decimal WeeklySalaryDecimal
+        {
+            get { return weeklySalaryDecimal; }
+            set {weeklySalaryDecimal = value; }
+        }
+
+        //One Method that is public.  Can be accessed from other classes
+        public string GetFullName()                  
+        {
+            return this.firstNameString + "  " + this.lastNameString;
+        }
+
+        public override string ToString()
+        {
+            return this.firstNameString + " " + this.lastNameString + " " + this.weeklySalaryDecimal.ToString("C");
+        }
+
+        //One method that is private.  Can only be called from inside this class
+        private string foo()
+        {
+            return "FOO";
+        }
+
+        // 3 parameter constructor
+        public Employee(string firstNameString, string lastNameString, decimal weeklySalaryDecimal)
+        {
+            //Assign the passed in values to the fields
+            this.firstNameString = firstNameString;
+            this.lastNameString = lastNameString;
+            //Assign the passed in value by the property
+            this.WeeklySalaryDecimal = weeklySalaryDecimal;
+        }
+
+        //Default constructor
+        public Employee()
+        {
+            //Left blank...For now...
+        }
+    }
+}
--- cis237inclass1/Program.cs
+++ cis237inclass1/Program.cs
@@ -9,9 +9,52 @@ namespace cis237inclass1
     class Program
     {
         static void Main(string[] args)
-        {            
-            Console.WriteLine("Zapdaboop");
-            Console.WriteLine("I'M A SCAT MAN!");
+        {
+            Employee employee1 = new Employee("Johnny", "MacEnroe", 1000000.00m);
+            Employee employee2 = new Employee("Johnny", "Goode", 25.00m);
+
+            int myNumberInt = 5;
+
+            Console.WriteLine(myNumberInt);
+            changeAnInt(myNumberInt);
+            Console.WriteLine(myNumberInt);
+
+            Console.WriteLine(employee1.ToString());
+            changeAnObject(employee1);
+            Console.WriteLine(employee1.ToString());
+
+
+            Console.WriteLine(employee1.GetFullName());
+            Console.WriteLine(employee2.ToString()); 
+          
+            //Showing how to use an array with objects
+            Employee[] employees = new Employee[10];
+
+            employees[0] = new Employee("Daryl", "Hammond", 255.55m);
+            employees[1] = new Employee("Austin", "Powers", 100.33m);
+            employees[2] = new Employee("Dustin", "Hoffman", 450.23m);
+            employees[3] = new Employee("Crazy", "Eyes", 0.01m);
+            employees[4] = new Employee("Billy", "Madison", 0.50m);
+            employees[5] = new Employee("Billy", "Madison", 0.50m);
+            employees[6] = new Employee("Billy", "Madison", 0.50m);
+            employees[7] = new Employee("Billy", "Madison", 0.50m);
+            employees[8] = new Employee("Billy", "Madison", 0.50m);
+            employees[9] = new Employee("Billy", "Madison", 0.50m);
+
+            foreach (Employee employee in employees)
+            {
+                Console.WriteLine(employee.ToString());
+            }
+        }
+
+        static void changeAnInt(int myNumberInt)
+        {
+            myNumberInt = 450;
+        }
+
+        static void changeAnObject(Employee employee)
+        {
+            employee.FirstNameString = "Thor";
         }
     }
 }
